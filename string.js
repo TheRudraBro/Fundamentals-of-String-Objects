b3de77f094b38aec06d7c08f597367e4c47861fa
TheRudraBro <rudramraj22@gmail.com>
2025-08-01 12:58:25 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,20 @@
+const country = 'Bangladesh';
+const division = "Dhaka";
+const district = `B-Baria`;
+// const thana = new String ('Demra');
+
+    // console.log(country);
+    // console.log(division);
+    // console.log(district);
+    // console.log(thana);
+
+// string is immutable not be changed
+// array is mutable can be changed
+    const numbers= [23,34,3,4,32,44,32];
+
+
+    const capital ='Dhaka';
+    // console.log(capital.length);
+    // console.log(capital[3]);
+
+    console.log(capital);
